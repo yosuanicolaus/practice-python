8a2d0cc6cf9ce095aee3b5e4e25dfb682bc496bb
Yosua Nicolaus <cryodawn257@gmail.com>
2022-06-19 16:54:48 +0700

    complete neural network class

--- neural_network.py
+++ neural_network.py
@@ -7,6 +7,18 @@ class NeuralNetwork:
         for i in range(len(self.levels)):
             self.levels[i] = Level(neuron_counts[i], neuron_counts[i + 1])
 
+    def feed_forward(self, new_inputs: list[float]) -> list[float]:
+        outputs = self.levels[0].feed_forward(new_inputs)
+
+        for i in range(1, len(self.levels)):
+            outputs = self.levels[i].feed_forward(outputs)
+
+        return outputs
+
+    def mutate(self, mutate_power: float) -> None:
+        for level in self.levels:
+            level.mutate(mutate_power)
+
     @staticmethod
     def print_values(nn):
         i = 0
@@ -29,9 +41,45 @@ class Level:
                     self.biases[o] = random.random() * 2 - 1
                 self.weights[i][o] = random.random() * 2 - 1
 
+    def feed_forward(self, new_inputs: list[float]) -> list[float]:
+        for i in range(len(self.inputs)):
+            self.inputs[i] = new_inputs[i]
+
+        for o in range(len(self.outputs)):
+            sum = 0
+            for i in range(len(self.inputs)):
+                sum += self.weights[i][o] * self.inputs[i]
+
+            if sum > self.biases[o]:
+                self.outputs[o] = 1
+            else:
+                self.outputs[o] = 0
+
+        return self.outputs
+
+    def mutate(self, mutate_power):
+        for i in range(len(self.inputs)):
+            for o in range(len(self.outputs)):
+                if i == 0:
+                    self.biases[o] = lerp(
+                        self.biases[o],
+                        random.random() * 2 - 1,
+                        mutate_power
+                    )
+
+                self.weights[i][o] = lerp(
+                    self.weights[i][o],
+                    random.random() * 2 - 1,
+                    mutate_power
+                )
+
     @staticmethod
     def print_values(level):
         print("BIAS")
         print(level.biases)
         print("WEIGHTS")
         print(level.weights)
+
+
+def lerp(a, b, t):
+    return a + (b - a) * t
